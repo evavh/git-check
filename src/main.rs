86459f83326809475e13e9fa4fd33c874f0a958d
Eva van Houten <opensource@evavh.net>
2024-07-01 12:00:46 +0200

    Add ignore_file flag, improve help

--- src/main.rs
+++ src/main.rs
@@ -12,26 +12,40 @@ mod status;
 
 const IGNORE_FILENAME: &str = "git_check_ignore";
 
-/// Utility to check the status of git repos within a directory
 #[derive(Parser, Debug)]
+#[command(
+    about = "Utility to check the status of all git repos within a directory"
+)]
 #[command(version)]
 struct Args {
-    /// Parent directory to check for git repos (default current dir)
+    /// Parent directory to check for git repos (default: current directory)
     #[arg(default_value=std::env::current_dir().unwrap().into_os_string())]
-    #[arg(hide_default_value=true)]
+    #[arg(hide_default_value = true)]
     path: PathBuf,
     /// Show the names of clean repos (instead of a count)
     #[arg(short = 'c', long)]
     show_clean: bool,
+    /// File in the parent directory that contains subdirectory
+    /// names to ignore
+    /// The hidden file (.FILENAME) will be checked if it doesn't
+    /// exist
+    /// (default: git_check_ignore / .git_check_ignore)
+    #[arg(short, long)]
+    #[arg(default_value=IGNORE_FILENAME)]
+    #[arg(hide_default_value = true)]
+    #[arg(value_name="FILENAME")]
+    #[clap(verbatim_doc_comment)]
+    ignore_file: String,
 }
 
 fn main() {
     let Args {
         path: base_path,
         show_clean,
+        ignore_file,
     } = Args::parse();
 
-    let ignored_paths = ignored_paths(&base_path);
+    let ignored_paths = ignored_paths(&ignore_file, &base_path);
     let repos: Vec<_> = fs::read_dir(base_path)
         .unwrap()
         .map(Result::unwrap)
@@ -61,9 +75,9 @@ fn main() {
     }
 }
 
-fn ignored_paths(base_path: &Path) -> Vec<PathBuf> {
-    let path = base_path.join(IGNORE_FILENAME);
-    let hidden_path = base_path.join(".".to_owned() + IGNORE_FILENAME);
+fn ignored_paths(ignore_filename: &str, base_path: &Path) -> Vec<PathBuf> {
+    let path = base_path.join(ignore_filename);
+    let hidden_path = base_path.join(".".to_owned() + ignore_filename);
 
     let file_contents = fs::read_to_string(path).unwrap_or_else(|_| {
         fs::read_to_string(hidden_path).unwrap_or_default()
